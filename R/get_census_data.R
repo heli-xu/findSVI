20d1c9db3366eae6002e8cb61f00aa1595e431a4
Heli Xu <xuheli91@gmail.com>
2023-04-11 20:14:49 -0400

    modify get_census_data and add documentation
    load() and get() .rda data.
    first check done. -to do: dataset documentation; dependencies.
    deleted initial hello related files.

--- R/get_census_data.R
+++ R/get_census_data.R
@@ -1,6 +1,33 @@
-get_census_data <- function(year, geography, state = NULL, ...){
+#' Retrieve census data for American Community Survey (with Tidycensus)
+#'
+#'@description This function uses `get_acs()`from [Tidycensus](https://cran.r-project.org/web/packages/tidycensus/index.html) with a pre-defined list of variables to retrieves ACS data for SVI calculation.
+#'Note that a Census API key is required for this function to work, which can be obtained at <https://api.census.gov/data/key_signup.html>. To set up your key, use `census_api_key("YOUR KEY GOES HERE")`, or include it as an argument.
+#'
+#' @param year The year of interest (available 2014-2021)
+#' @param geography The geography of interest (eg. state, county, zcta, tract)
+#' @param state (Optional) Specify the state of interest. If data for multiple states are retrieved together, ranking for SVI calculation will be performed among all states.
+#' @param county (Optional) Specify the county(s) of interest, must be combined with a value supplied to "state".
+#' @param key Your Census API key.
+#' @param ... Other arguments; more details please see `?get_acs()`
+#'
+#' @return A tibble of ACS data with each row represents an enumeration unit and each column represents the variables ("wide" form).
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' census_api_key("YOUR KEY GOES HERE")
+#' get_census_data(2018, "county", "PA")}
 
-  var_list <- readRDS(paste0("data/census_variables_",year,".rda")) %>%
+get_census_data <- function(year,
+  geography,
+  state = NULL,
+  county = NULL,
+  key = NULL,
+  ...)
+{
+  var_list <-
+    load(paste0("../data/census_variables_", year, ".rda")) %>%
+    get() %>%
     unlist() %>%
     unname()
 
