18843a8a8b194b8c3afe00bf8dd8bd798f4f6fdb
Heli Xu <xuheli91@gmail.com>
2024-07-02 13:20:42 -0400

    add zcta_state_xwalk2022 for getting zcta data by state #24

--- data-raw/zcta_state_xwalk_2019-21.R
+++ data-raw/zcta_state_xwalk_2019-21.R
@@ -7,10 +7,106 @@ library(tidyverse)
 library(janitor)
 library(stringr)
 
+# zcta_state_xwalk2022---------------------------------
+
+## zcta-county-----------------------------
+#same as 2020
+zcta_cty20 <- read_csv("xls/zcta_county_2020_geocorr.csv") %>%
+  slice(-1) %>%
+  clean_names()
+
+st_abb <- zcta_cty20 %>%
+  select(GEOID = county, county_name) %>%
+  mutate(st_code = str_sub(GEOID, 1, 2),
+    st_abb = str_sub(county_name, -2)) %>%
+  select(st_code, st_abb) %>%
+  distinct(st_code, .keep_all = TRUE)
+
+
+## county-state--------------------
+cty_st22 <- read_xlsx("xls/all-geocodes-v2022.xlsx", skip = 4) %>%
+  #skip first 4 rows (table titles..)
+  clean_names()
+# change to county-equivalents in CT for 2022
+#https://www.census.gov/programs-surveys/acs/technical-documentation/user-notes/2023-01.html
+#only affect 1 year ACS, 5 year ACS still follows prior2022 boundaries
+
+### modify CT-----------
+cty_st21_ct <- read_xlsx("xls/all-geocodes-v2021.xlsx", skip = 4) %>%
+  clean_names() %>%
+  filter(state_code_fips == "09")
+
+#this double assign just to make column names auto complete
+cty_st21_ct <- cty_st21_ct %>%
+  rename(
+    state_fips_code = state_code_fips,
+    county_fips_code = county_code_fips,
+    county_subdivision_fips_code = county_subdivision_code_fips,
+    place_fips_code = place_code_fips,
+    consolidtated_city_fips_code = consolidtated_city_code_fips,
+    area_name = area_name_including_legal_statistical_area_description
+    )
+
+cty_st22_ct <- cty_st22 %>%
+  filter(!state_fips_code == "09") %>%
+  bind_rows(cty_st21_ct)
+
+#join state name and abbr
+add_state <- cty_st22_ct %>%
+  distinct(state_fips_code, .keep_all = TRUE) %>%
+  select(st_code = state_fips_code,
+    state = area_name) %>% #col names updated
+  left_join(st_abb, by = "st_code") %>%
+  drop_na()
+
+
+#add state abb and full name to county-state
+cty_st22_clean <- cty_st22_ct %>%
+  rename(st_code = state_fips_code) %>%
+  mutate(GEOID = paste0(st_code, county_fips_code)) %>%
+  left_join(add_state, by = "st_code") %>%
+  select(st_code,
+    state,
+    GEOID,
+    county = area_name,
+    st_abb)
+
+cty_st22_clean %>% count(state)
+#53 rows with 1 NA (whole US)
+# turn out the same as 2021
+
+diff <- cty_st22_clean %>%
+  select(GEOID, st_code, county) %>%
+  left_join(cty_st21_clean %>%
+      select(GEOID, st_code, county),
+    by = c("st_code","county")
+) %>%
+  mutate(diff = as.numeric(GEOID.x)-as.numeric(GEOID.y))
+#some of the "city" has two GEOID, like 51000 and 51840
+
+## join zcta-county-state----------
+zcta_state_xwalk2022 <- zcta_cty20 %>%
+  select(GEOID = county, ZCTA = zcta, county_name) %>%
+  #GEOID(county) already character and right length with pad"0"
+  left_join(cty_st22_clean, by = "GEOID", multiple = "all") %>%
+  #there will be multimatch
+  drop_na() %>%
+  select(ZCTA, st_code, county, state, st_abb) %>%
+#not selecting GEOID to avoid confusion, pulled data(for zcta) GEOID is ZCTA
+  distinct()
+
+zcta_state_xwalk2022 <- zcta_state_xwalk2022 %>%
+  mutate(st_code = as.numeric(st_code))
+
+#check how many states
+z <- zcta_state_xwalk2022 %>% group_by(state) %>% count()
+
+usethis::use_data(zcta_state_xwalk2022, overwrite = TRUE)
+
 # zcta_state_xwalk2021 ----------------------------------------------------
 
-#zcta to county relationship file(2020)
-#
+##zcta-county relationship file(2020)------
+
 #zcta_cty20  <- read.table("xls/zcta_county_2020.txt", sep = "|", header = T)
 #only has 9695 rows (ZCTAs), matching to 19 states only
 #use Geocorr2022 data instead:
@@ -27,7 +123,7 @@ st_abb <- zcta_cty20 %>%
   select(st_code, st_abb) %>%
   distinct(st_code, .keep_all = TRUE)
 
-#county to state reference file(2021)
+##county-state reference file(2021) ---------
 #https://www.census.gov/programs-surveys/popest/geographies/reference-files.html
 cty_st21 <- read_xlsx("xls/all-geocodes-v2021.xlsx", skip = 4) %>% #skip first 4 rows (table titles..)
   clean_names()
@@ -52,6 +148,7 @@ cty_st21_clean <- cty_st21 %>%
     county = area_name_including_legal_statistical_area_description,
     st_abb)
 
+cty_st21_clean %>% count(state)
 
 zcta_state_xwalk2021 <- zcta_cty20 %>%
   select(GEOID = county, ZCTA = zcta, county_name) %>%
@@ -65,14 +162,17 @@ zcta_state_xwalk2021 <- zcta_state_xwalk2021 %>%
   mutate(st_code = as.numeric(st_code))
 
 #check how many states
-z <- zcta_state_xwalk2021 %>% group_by(state) %>% count()
+x <- zcta_state_xwalk2021 %>% group_by(state) %>% count()
+
+#check repeats
+zcta_state_xwalk2021 <- zcta_state_xwalk2021 %>% distinct()
 
 usethis::use_data(zcta_state_xwalk2021, overwrite = TRUE)
 
 
 # zcta_state_2020 ---------------------------------------------------------
 
-
+### ZCTA-county--------------
 #relationship file only has 9695 zcta entries, matched to 22 states, not complete
 #zcta_cty20  <- read.table("xls/zcta_county_2020.txt", sep = "|", header = T)
 #here use https://mcdc.missouri.edu/applications/geocorr2022.html
@@ -88,6 +188,7 @@ st_abb <- zcta_cty20 %>%
   select(st_code, st_abb) %>%
   distinct(st_code, .keep_all = TRUE)
 
+## county-state-----------------
 #county to state reference file(2020):
 #https://www.census.gov/programs-surveys/popest/geographies/reference-files.html
 cty_st20 <- read_xlsx("xls/all-geocodes-v2020.xlsx", skip = 4) %>% #skip first 4 rows (table titles..)
@@ -111,7 +212,7 @@ cty_st20_clean <- cty_st20 %>%
     county = area_name_including_legal_statistical_area_description,
     st_abb)
 
-
+## join ZCTA-county-state--------------------
 zcta_state_xwalk2020 <- zcta_cty20 %>%
   select(GEOID = county, ZCTA = zcta, county_name) %>%
   #GEOID(county) already character and right length with pad"0"
@@ -127,12 +228,14 @@ zcta_state_xwalk2020 <- zcta_state_xwalk2020 %>%
 #checking how many states included (52)
 y2 <- zcta_state_xwalk2020 %>% group_by(state) %>% count()
 
+#check repeats
+zcta_state_xwalk2020 <- zcta_state_xwalk2020 %>% distinct()
+
 usethis::use_data(zcta_state_xwalk2020, overwrite = TRUE)
 
 
-
 # zcta_state_xwalk2019 ----------------------------------------------------
-
+## ZCTA-county----------------
 #zcta to county relationship file(2010)
 #https://www.census.gov/geographies/reference-files/time-series/geo/relationship-files.2010.html#list-tab-1709067297
 zcta_cty10 <-
@@ -140,6 +243,9 @@ zcta_cty10 <-
     sep = ",",
     header = T)
 
+
+
+## county-state---------------
 #county to state reference file(2019)
 #https://www.census.gov/programs-surveys/popest/geographies/reference-files.html
 cty_st19 <-
@@ -166,7 +272,7 @@ cty_st19_clean <- cty_st19 %>%
     county = area_name_including_legal_statistical_area_description,
     st_abb)
 
-
+## join ZCTA-county-state---------
 zcta_state_xwalk2019 <- zcta_cty10 %>%
   select(ZCTA5, GEOID) %>%
   ##Below step VERY important!! state code of 01 will be 1 and not matching.
@@ -187,8 +293,10 @@ zcta_state_xwalk2019 <- zcta_state_xwalk2019 %>%
 #check how many states
 x <- zcta_state_xwalk2019 %>% group_by(state) %>% count()
 
-usethis::use_data(zcta_state_xwalk2019, overwrite = TRUE)
+#check repeats
+zcta_state_xwalk2019 <- zcta_state_xwalk2019 %>% distinct()
 
+usethis::use_data(zcta_state_xwalk2019, overwrite = TRUE)
 
 
 # Extra: valid state table ----------------------------------------------
