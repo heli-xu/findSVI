f2c5f08333a01be57fb86c69aa5a9714bcff678d
Heli Xu <xuheli91@gmail.com>
2025-02-07 21:15:08 -0500

    update doi format in documentation-per cran suggestion author (year) <doi:xx>

--- R/cty_cz_2020_xwalk.R
+++ R/cty_cz_2020_xwalk.R
@@ -3,9 +3,9 @@
 #' This table contains GEOIDs for US counties and the commuting zones they are
 #' nested in. Commuting zones can be used to study regional economy with
 #' considerations of urban-rural interconnections across state lines. For
-#' details refer to papers by  [Fowler Jensen and Rhubart
-#' (2016)](\doi{10.1007/s11113-016-9386-0}) and [Fowler
-#' (2024)](\doi{10.17605/OSF.IO/J256U}).
+#' details refer to papers by  Fowler Jensen and Rhubart
+#' (2016) <doi:10.1007/s11113-016-9386-0> and Fowler
+#' (2024) <doi:10.17605/OSF.IO/J256U>.
 #'
 #' @name cty_cz_2020_xwalk
 #' @format ## A data frame with 2 columns and 3222 rows:
