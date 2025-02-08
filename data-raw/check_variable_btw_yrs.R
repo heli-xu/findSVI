2039a42c429b0014a1306747b5d9b5a12866249d
Heli Xu <xuheli91@gmail.com>
2025-02-08 15:11:46 -0500

    check var pre-2012 (internal function)

--- data-raw/check_variable_btw_yrs.R
+++ data-raw/check_variable_btw_yrs.R
@@ -6,26 +6,30 @@ library(tidyverse)
 check_variable <- function(year1, year2, dataset){
   var1 <- load_variables(year1, dataset)
   var2 <- load_variables(year2, dataset)
-  var_to_check <- readRDS(paste0("data/census_variables_",year1,".rds")) %>% 
-    unlist() %>% 
-    unname() %>% 
+  var_to_check <- get(paste0("census_variables_",year1)) %>%
+    unlist() %>%
+    unname() %>%
     str_sub(1L, -2L)
-  
-  var1_2 <- var1 %>% 
-    filter(name%in%all_of(var_to_check)) %>% 
-    select(name, label) %>% 
+
+  var1_2 <- var1 %>%
+    filter(name%in%all_of(var_to_check)) %>%
+    select(name, label) %>%
     left_join(
-      var2 %>% 
+      var2 %>%
         filter(name%in%all_of(var_to_check)),
       by = "name"
     )
-  
+
   return(var1_2)
 }
 
 var1_2 <- check_variable(2020, 2021, "acs5/profile")
 acs_var_1_2 <- check_variable(2020, 2021, "acs5")
 sub_var_1_2 <- check_variable(2020, 2021, "acs5/subject")
+
+var1_2 <- check_variable(2012, 2009, "acs5/profile")
+acs_var_1_2 <- check_variable(2012, 2009, "acs5")
+sub_var_1_2 <- check_variable(2012, 2009, "acs5/subject")
 
 var1_2$label.x == var1_2$label.y
 #sometimes it's format that causes "unequal"
